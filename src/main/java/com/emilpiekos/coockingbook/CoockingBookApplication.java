21808cfdc49b08a00bf629878aa5caef0f69148b
Emil Piękoś <piekosemil@gmail.com>
2024-09-09 11:30:21 +0200

    Initial commit

---
+++ src/main/java/com/emilpiekos/coockingbook/CoockingBookApplication.java
@@ -0,0 +1,13 @@
+package com.emilpiekos.coockingbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoockingBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoockingBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emilpiekos/coockingbook/CoockingBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emilpiekos.coockingbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoockingBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
